3e53162934b870d958d569317a1b9b6bd83835f0
ehmasuk <ehmasuk@gmail.com>
2024-09-26 00:46:35 +0600

    limit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 const cloudinary = require("./cloudinary");
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 const mongoose = require("mongoose");
